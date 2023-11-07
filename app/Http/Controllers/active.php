8e8ee65432b6ea4eec0bba0de23ef339d81182c8
Morad0099 <musahmorad01@gmail.com>
2023-11-07 13:35:33 +0000

    start commit

---
+++ app/Http/Controllers/active.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class active extends Controller
+{
+    public function index(){
+        
+    }
+}
---
+++ resources/views/landpage.blade.php
@@ -0,0 +1,12 @@
+<x-header>
+</x-header>
+<x-navbar>
+</x-navbar>
+@include('partials._hero')
+<x-about>
+</x-about>
+<x-contact>
+</x-contact>
+<x-footer>
+</x-footer>
+
